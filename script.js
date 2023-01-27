d15878791097861c2fa260a3a97bb37eb4d018b1
Mohammad Abdollahzadeh <102470936+MohammadAbdollahzade@users.noreply.github.com>
2023-01-28 01:12:33 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
-let slide = document.querySelector('.slide')
+const slide = document.querySelector('.slide')
+const chevronLeft = document.querySelector('.chevron-left')
+const chevronRight = document.querySelector('.chevron-right')
+
 let images = ['img1.jpg','img2.jpg','img3.jpg']
 
 let i = 1
 
+let chevronSlide = false
+
+//autoplay 
+
 function autoPlay (){
-    if(i == images.length){
+    if (!chevronSlide) {   
+        if(i == images.length){
+            i = 0
+        }
+        slide.style.backgroundImage = 'url(' + images[i] + ')'
+        i++
+    }
+    chevronSlide = false
+}
+
+setInterval(autoPlay,3000)
+
+//chevron
+
+chevronLeft.addEventListener('click',()=>{
+    chevronLeft.style.cssText = 'animation: chevron 0.5s;'
+    chevronSlide = true
+    i--
+    if(i < 0){
+        i = 2
+    }
+    slide.style.backgroundImage = 'url(' + images[i] + ')'
+})
+
+chevronRight.addEventListener('click',()=>{
+    chevronRight.style.cssText = 'animation: chevron 0.5s;'
+    chevronSlide = true
+    i++
+    if(i > 2){
         i = 0
     }
     slide.style.backgroundImage = 'url(' + images[i] + ')'
-    i++
-}
+})
 
-setInterval(autoPlay,2000)
+setInterval(()=>{
+    chevronLeft.style.cssText = 'animation: 0;'
+    chevronRight.style.cssText = 'animation: 0;'
+},200)
